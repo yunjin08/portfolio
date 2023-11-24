ab03d1562e97d4cebb6cc2678bcd2dedae956428
Yunjin <jed.donaire08@gmail.com>
2023-11-24 12:58:02 +0800

    Added LearnMore

--- app/portfolio/page.jsx
+++ app/portfolio/page.jsx
@@ -8,6 +8,7 @@ function Portfolio() {
   const [all, setAll] = useState(true);
   const [frontend, setFrontend] = useState(false);
   const [fullstack, setFullstack] = useState(false);
+  const [projectHovered, setProjectHovered] = useState(null);
 
   const [fade, setFade] = useState(false);
 
@@ -17,6 +18,14 @@ function Portfolio() {
     const timeout = setTimeout(() => setFade(false), 300);
     return () => clearTimeout(timeout);
   }, [all, frontend, fullstack]);
+
+  const handleHover = (index) => {
+    setProjectHovered(index);
+  };
+
+  const handleMouseLeave = () => {
+    setProjectHovered(null);
+  };
 
   return (
     <section>
@@ -71,14 +80,30 @@ function Portfolio() {
               fade ? "fade-in" : ""
             }`}
           >
-            {projects.map((projects) => (
-              <div className="imagedet" key={projects.name}>
+            {projects.map((projects, index) => (
+              <div
+                className="imagedet"
+                onMouseEnter={() => handleHover(index)}
+                onMouseLeave={handleMouseLeave}
+                key={projects.name}
+              >
+                {projectHovered === index && (
+                  <div className="absolute top-[50%] left-[50%] z-[100] transform -translate-x-1/2 -translate-y-1/2 ">
+                    <div className="flex flex-col w-full items-center justify-center">
+                      <h1 className="text-4xl font-medium">{projects.title}</h1>
+                      <p className="text-3xl pt-3 pb-20">{projects.stack}</p>
+                      <button className="w-[20rem] text-4xl  h-[5rem] border-2  border-[#00abf0] hover:cursor-pointer hover:bg-[#00abf0] hover:text-[#112e42]">
+                        LEARN MORE
+                      </button>
+                    </div>
+                  </div>
+                )}
                 <Image
                   src={projects.folder}
                   alt={projects.name}
                   width={1920}
                   height={1080}
-                  className="object-cover w-full h-full  border-4 border-[#00abf0] hover:cursor-pointer hover:opacity-[0.2]"
+                  className="object-cover w-full h-full  border-4 border-[#00abf0]  hover:opacity-[0.2]"
                 />
               </div>
             ))}
@@ -92,9 +117,29 @@ function Portfolio() {
             }`}
           >
             {projects.map(
-              (projects) =>
+              (projects, index) =>
                 projects.type === "frontend" && (
-                  <div className="imagedet" key={projects.name}>
+                  <div
+                    className="imagedet"
+                    onMouseEnter={() => handleHover(index)}
+                    onMouseLeave={handleMouseLeave}
+                    key={projects.name}
+                  >
+                    {projectHovered === index && (
+                      <div className="absolute top-[50%] left-[50%] z-[100] transform -translate-x-1/2 -translate-y-1/2 ">
+                        <div className="flex flex-col w-full items-center justify-center">
+                          <h1 className="text-4xl font-medium">
+                            {projects.title}
+                          </h1>
+                          <p className="text-3xl pt-3 pb-20">
+                            {projects.stack}
+                          </p>
+                          <button className="w-[20rem] text-4xl  h-[5rem] border-2  border-[#00abf0] hover:cursor-pointer hover:bg-[#00abf0] hover:text-[#112e42]">
+                            LEARN MORE
+                          </button>
+                        </div>
+                      </div>
+                    )}
                     <Image
                       src={projects.folder}
                       alt={projects.name}
@@ -114,9 +159,29 @@ function Portfolio() {
             }`}
           >
             {projects.map(
-              (projects) =>
+              (projects, index) =>
                 projects.type === "fullstack" && (
-                  <div className="imagedet" key={projects.name}>
+                  <div
+                    className="imagedet"
+                    onMouseEnter={() => handleHover(index)}
+                    onMouseLeave={handleMouseLeave}
+                    key={projects.name}
+                  >
+                    {projectHovered === index && (
+                      <div className="absolute top-[50%] left-[50%] z-[100] transform -translate-x-1/2 -translate-y-1/2 ">
+                        <div className="flex flex-col w-full items-center justify-center">
+                          <h1 className="text-4xl font-medium">
+                            {projects.title}
+                          </h1>
+                          <p className="text-3xl pt-3 pb-20">
+                            {projects.stack}
+                          </p>
+                          <button className="w-[20rem] text-4xl  h-[5rem] border-2  border-[#00abf0] hover:cursor-pointer hover:bg-[#00abf0] hover:text-[#112e42]">
+                            LEARN MORE
+                          </button>
+                        </div>
+                      </div>
+                    )}
                     <Image
                       src={projects.folder}
                       alt={projects.name}
---
+++ components/Posts.jsx
@@ -0,0 +1,9 @@
+"use client";
+import { projects } from "/files";
+import { useState } from "react";
+
+function Posts() {
+  return <div>Posts</div>;
+}
+
+export default Posts;
