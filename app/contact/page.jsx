0f9bac1035f16ad18275e760902877f04ce28b89
Yunjin <jed.donaire08@gmail.com>
2023-11-27 23:10:27 +0800

    Added Contacts

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-function page() {
-  return <div>page</div>;
+function Page() {
+  return (
+    <section>
+      <div className="flex flex-col items-center justify-center -mt-10 md:mt-auto">
+        <h1 className="abme text-6xl md:text-8xl font-bold mb-10 md:mb-20">
+          Contact <span>Me!</span>{" "}
+        </h1>
+      </div>
+    </section>
+  );
 }
 
-export default page;
+export default Page;
