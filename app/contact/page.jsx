5c2658d24135cb68ad83ac27e776987ec915965a
Yunjin <jed.donaire08@gmail.com>
2023-11-27 23:47:56 +0800

    Put a placeholder

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,6 +1,15 @@
+"use client";
+
 import React from "react";
+import Input from "<jed>/components/Input";
+import { useState } from "react";
 
 function Page() {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
   return (
     <section>
       <div className="flex flex-col items-center justify-center -mt-10 md:mt-auto">
@@ -8,15 +17,28 @@ function Page() {
           Contact <span>Me!</span>{" "}
         </h1>
         <div className="flex w-[60%] bg-red-500 flex-col h-[40rem]">
-          <div className="flex w-full items-center justify-center h-full">
-            <div className="w-[45%] bg-pink-300 h-[10%]">Fullname</div>
-            <div className="w-[45%] h-5">Email Address</div>
+          <div className="flex w-full items-center justify-center h-[10rem] gap-10">
+            <Input placeholder={"Fullname"} />
+            <Input placeholder={"Email Address"} />
           </div>
-          <div className="flex w-full items-center justify-center h-full">
-            <div className="w-[45%] bg-pink-300 h-[10%]">Mobile Number</div>
-            <div className="w-[45%] h-5">Email Subject</div>
+          <div className="flex w-full items-center justify-center h-[10rem]  gap-10">
+            <Input placeholder={"Mobile Number"} />
+            <Input placeholder={"Email Subject"} />
           </div>
-          <input type="text" placeholder="Your Message" />
+          <div className="w-full relative h-[45%]">
+            <input
+              type="text"
+              placeholder=""
+              className="mb-2 w-[95%] ml-6 h-full  "
+            />
+            <label
+              className={`absolute top-0 left-6 text-gray-500 pointer-events-none ${
+                inputValue ? "hidden" : ""
+              }`}
+            >
+              Your Message
+            </label>
+          </div>
         </div>
       </div>
     </section>
---
+++ components/Input.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Input({ placeholder }) {
+  return (
+    <input
+      type="text"
+      placeholder="Email Address"
+      className="w-[44%] h-5"
+    ></input>
+  );
+}
+
+export default Input;
