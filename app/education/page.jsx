2e0ff93f571b1d0432c2110f5b1e60868c3d64ce
Yunjin <jed.donaire08@gmail.com>
2023-12-02 22:33:07 +0800

    Added States

--- app/education/page.jsx
+++ app/education/page.jsx
@@ -1,7 +1,30 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
+import { useRef, useState } from "react";
 
-function page() {
+function Page() {
+  const [certActive1, certToActive1] = useState(false);
+  const [certActive2, certToActive2] = useState(false);
+  const [certActive3, certToActive3] = useState(false);
+  const cert1 = useRef(null);
+  const cert2 = useRef(null);
+  const cert3 = useRef(null);
+
+  {
+    /* Handling Certificates Activation and Referencing */
+  }
+
+  const scrollToCert1 = () => {
+    cert1.current.scrollIntoView({ behavior: "smooth" });
+  };
+  const scrollToCert2 = () => {
+    cert2.current.scrollIntoView({ behavior: "smooth" });
+  };
+  const scrollToCert3 = () => {
+    cert3.current.scrollIntoView({ behavior: "smooth" });
+  };
   return (
     <section>
       <div className="posting flex items-center justify-center flex-col">
@@ -64,7 +87,10 @@ function page() {
 
             <div className="ed flex flex-col space-y-16 max-w-[650px] pl-10 h-full">
               <div className="precontent">
-                <div className="content  rounded-xl space-y-2  p-10 relative ">
+                <div
+                  className="content  rounded-xl space-y-2  p-10 relative hover:cursor-pointer"
+                  onClick={scrollToCert1}
+                >
                   <p className="date text-[1.7rem]">&#9634; 2022</p>
                   <p className="font-semibold text-[1.5xl] sm:text-[1.7rem] lg:text-[1.9rem]">
                     Career Series 2022: Industry Talks: Telecommunications and
@@ -73,7 +99,10 @@ function page() {
                 </div>
               </div>
               <div className="precontent">
-                <div className="content rounded-xl space-y-2  p-10 relative">
+                <div
+                  className="content rounded-xl space-y-2  p-10 relative hover:cursor-pointer"
+                  onClick={scrollToCert1}
+                >
                   <p className="date text-[1.7rem]">&#9634; 2022</p>
                   <p className="font-semibold text-[1.5xl] sm:text-[1.7rem] lg:text-[1.9rem]">
                     Career Series 2022: Industry Talks: Technology
@@ -82,7 +111,10 @@ function page() {
               </div>
 
               <div className="precontent">
-                <div className="content rounded-xl space-y-2  p-10 relative">
+                <div
+                  className="content rounded-xl space-y-2  p-10 relative hover:cursor-pointer"
+                  onClick={scrollToCert1}
+                >
                   <p className="date text-[1.7rem]">&#9634; 2022</p>
                   <p className="font-semibold text-[1.5xl] sm:text-[1.7rem] lg:text-[1.9rem]">
                     Career Series 2022: Industry Talks: Start-up /NGO
@@ -103,11 +135,13 @@ function page() {
               alt="jed"
               width={410}
               height={110}
+              ref={cert1}
               className="about-img object-cover rounded-lg hover:scale-[1.05] transition "
             />
             <Image
               src="/start.png"
               alt="jed"
+              ref={cert2}
               width={410}
               height={110}
               className="about-img object-cover rounded-lg hover:scale-[1.05] transition"
@@ -117,6 +151,7 @@ function page() {
             <Image
               src="/tech.png"
               alt="jed"
+              ref={cert3}
               width={410}
               height={110}
               className="about-img object-cover rounded-lg hover:scale-[1.05] transition"
@@ -128,4 +163,4 @@ function page() {
   );
 }
 
-export default page;
+export default Page;
