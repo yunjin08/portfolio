955eb1db378ccc0955cde0ef0108ad5aece891ac
Yunjin <jed.donaire08@gmail.com>
2024-01-01 21:57:30 +0800

    restyle footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,41 +5,26 @@ import Image from "next/image";
 function Footer() {
   const pathname = usePathname();
   return (
-    <div className="flex relative items-center flex-col  justify-center w-full h-32 foter bg-[#112e42] ">
-      <nav className="flex relative flex-1 max-w-[550px] items-center justify-evenly space-x-3 md:space-x-5 text-[1.3rem] sm:text-[1.7rem] font-medium">
-        <Link className={`link ${pathname === "/" ? "active" : ""}`} href="/">
-          Home
+    <div className="flex relative  items-center justify-between px-[4rem]  md:px-[13.5rem] w-full h-36 foter border-t-2 border-[#19374c] bg-[#112e42]">
+      <div className="flex relative  items-center   gap-2 md:gap-10 justify-start  ">
+        <Link href="/" className="flex ">
+          {" "}
+          <Image
+            src="/favicon/favicon.ico"
+            alt="favicon"
+            width={120}
+            height={180}
+            className="w-[3.5rem] md:w-[4rem]"
+          />
         </Link>
-        <Link
-          className={`link ${pathname === "/about" ? "active" : ""}`}
-          href="/about"
-        >
-          About
-        </Link>
-        <Link
-          className={`link ${pathname === "/education" ? "active" : ""}`}
-          href="/education"
-        >
-          Education
-        </Link>
-        <Link
-          className={`link ${pathname === "/portfolio" ? "active" : ""}`}
-          href="/portfolio"
-        >
-          Portfolio
-        </Link>
-        <a
-          href="resume.pdf"
-          className={`link ${pathname === "/contact" ? "active" : ""}`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Resume
-        </a>
-      </nav>
-      <h3 className="text-sm md:text-xl text-center">
-        &#169; 2023 jed.donaire08@gmail.com{" "}
-      </h3>
+        <h3 className="text-md font-serif md:text-2xl text-center">
+          &#169;JED - 2023{" "}
+        </h3>
+      </div>
+      <div className="flex flex-col gap-2 text-md sm:text-lg md:text-xl font-serif">
+        <p>Email me @ jed.donaire@gmail.com</p>
+        <p>Call me @ 09662781586</p>
+      </div>
     </div>
   );
 }
