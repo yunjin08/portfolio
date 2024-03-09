7fbd7a860275022b4c5f593a93aa00215ff5fc0f
Yunjin <jed.donaire08@gmail.com>
2024-03-09 23:57:50 +0800

    refactor: change header modal for projects

--- app/portfolio/page.jsx
+++ app/portfolio/page.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import Image from "next/image";
 import { projects } from "/files";
 import Posts from "<jed>/components/Posts";
+import ChangeStackModal from "<jed>/components/generics/ChangeStackModal";
 
 function Portfolio() {
   const [all, setAll] = useState(true);
@@ -65,46 +66,14 @@ function Portfolio() {
           <div className="mt-4 md:mt-8 w-[9rem] border-t-4" />
         </div>
         <div className={`${post && "hidden"}`}>
-          <div className="w-full flex mt-[5rem] text-2xl sm:text-4xl md:text-5xl xl:text-[3.5rem] font-medium items-center justify-center gap-1 sm:gap-5">
-            <div
-              onClick={() => {
-                setAll(true);
-                setFrontend(false);
-                setFullstack(false);
-              }}
-              className={`text-center w-[8rem] sm:w-[16rem]  md:w-[20rem] xl:w-[24rem] hover:cursor-pointer ${
-                all && "bg-[#112e42] border-[#00abf0] object-cover border-2"
-              }`}
-            >
-              All
-            </div>
-            <div
-              onClick={() => {
-                setFrontend((prevState) => !prevState);
-                setAll(false);
-                setFullstack(false);
-              }}
-              className={`text-center w-[8rem] sm:w-[16rem]  md:w-[20rem] xl:w-[24rem] hover:cursor-pointer ${
-                frontend &&
-                "bg-[#112e42] border-[#00abf0] object-cover border-2"
-              }`}
-            >
-              Frontend
-            </div>
-            <div
-              onClick={() => {
-                setFullstack((prevState) => !prevState);
-                setAll(false);
-                setFrontend(false);
-              }}
-              className={`text-center w-[8rem] sm:w-[16rem] md:w-[20rem] xl:w-[24rem] hover:cursor-pointer ${
-                fullstack &&
-                "bg-[#112e42] border-[#00abf0] object-cover border-2"
-              }`}
-            >
-              Full Stack
-            </div>
-          </div>
+          <ChangeStackModal
+            setAll={setAll}
+            setFrontend={setFrontend}
+            setFullstack={setFrontend}
+            all={all}
+            frontend={frontend}
+            fullstack={fullstack}
+          />
         </div>
 
         {all && (
