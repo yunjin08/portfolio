517afb54950fb9c393ba21012456548ca5c5b4a8
Yunjin <jed.donaire08@gmail.com>
2024-03-10 12:51:27 +0800

    fix: fix the bug of header button

--- app/portfolio/page.jsx
+++ app/portfolio/page.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import Image from "next/image";
 import { projects } from "/files";
 import Posts from "<jed>/components/Posts";
+import ProjectsContent from "<jed>/components/generics/ProjectsContent";
 import ChangeStackModal from "<jed>/components/generics/ChangeStackModal";
 
 function Portfolio() {
@@ -66,145 +67,88 @@ function Portfolio() {
           <div className="mt-4 md:mt-8 w-[9rem] border-t-4" />
         </div>
         <div className={`${post && "hidden"}`}>
-          <ChangeStackModal
-            setAll={setAll}
-            setFrontend={setFrontend}
-            setFullstack={setFrontend}
-            all={all}
-            frontend={frontend}
-            fullstack={fullstack}
-          />
+          <div className="w-full flex mt-[5rem] text-2xl sm:text-4xl md:text-5xl xl:text-[3.5rem] font-medium items-center justify-center gap-1 sm:gap-5">
+            <div
+              onClick={() => {
+                setAll(true);
+                setFrontend(false);
+                setFullstack(false);
+              }}
+              className={`text-center w-[8rem] sm:w-[16rem]  md:w-[20rem] xl:w-[24rem] hover:cursor-pointer ${
+                all && "bg-[#112e42] border-[#00abf0] object-cover border-2"
+              }`}
+            >
+              All
+            </div>
+            <div
+              onClick={() => {
+                if (!frontend) {
+                  setFrontend((prevState) => !prevState);
+                  setAll(false);
+                  setFullstack(false);
+                }
+              }}
+              className={`text-center w-[8rem] sm:w-[16rem]  md:w-[20rem] xl:w-[24rem] hover:cursor-pointer ${
+                frontend &&
+                "bg-[#112e42] border-[#00abf0] object-cover border-2"
+              }`}
+            >
+              Frontend
+            </div>
+            <div
+              onClick={() => {
+                if (!fullstack) {
+                  setFullstack((prevState) => !prevState);
+                  setAll(false);
+                  setFrontend(false);
+                }
+              }}
+              className={`text-center w-[8rem] sm:w-[16rem] md:w-[20rem] xl:w-[24rem] hover:cursor-pointer ${
+                fullstack &&
+                "bg-[#112e42] border-[#00abf0] object-cover border-2"
+              }`}
+            >
+              Full Stack
+            </div>
+          </div>
         </div>
 
         {all && (
-          <div
-            className={`grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:w-[120rem] mt-24 gap-7 px-10 md:px-0 mb-44 ${
-              fade ? "fade-in" : ""
-            } ${post && "hidden"}`}
-          >
-            {projects.map((projects, index) => (
-              <div
-                className="imagedet"
-                onMouseEnter={() => handleHover(index)}
-                onMouseLeave={handleMouseLeave}
-                key={projects.name}
-              >
-                {projectHovered === index && (
-                  <div className="absolute top-[50%] left-[50%] z-[100] transform -translate-x-1/2 -translate-y-1/2 ">
-                    <div className="posting flex flex-col w-full items-center justify-center">
-                      <h1 className="text-4xl font-medium">{projects.title}</h1>
-                      <p className="text-3xl pt-3 pb-20">{projects.stack}</p>
-                      <button
-                        className="w-[20rem] text-4xl  h-[5rem] border-2  border-[#00abf0] object-cover hover:cursor-pointer hover:bg-[#00abf0] hover:text-[#112e42]"
-                        onClick={() => handlePosts(projects)}
-                      >
-                        LEARN MORE
-                      </button>
-                    </div>
-                  </div>
-                )}
-                <Image
-                  src={projects.folder}
-                  alt={projects.name}
-                  width={1920}
-                  height={1080}
-                  className="w-[45rem] h-[20rem] sm:h-[25rem] object-cover border-[0.2px] border-[#00abf0] object-cover  hover:opacity-[0.2]"
-                />
-              </div>
-            ))}
-          </div>
+          <ProjectsContent
+            post={post}
+            projects={projects}
+            handleMouseLeave={handleMouseLeave}
+            projectHovered={projectHovered}
+            handlePosts={handlePosts}
+            handleHover={handleHover}
+            projectType="all"
+            fade={fade}
+          />
         )}
 
         {frontend && (
-          <div
-            className={`grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:w-[120rem] mt-24 gap-7 px-10 md:px-0 mb-44  ${
-              fade ? "fade-in" : ""
-            } ${post && "hidden"}`}
-          >
-            {projects.map(
-              (projects, index) =>
-                projects.type === "frontend" && (
-                  <div
-                    className="imagedet"
-                    onMouseEnter={() => handleHover(index)}
-                    onMouseLeave={handleMouseLeave}
-                    key={projects.name}
-                  >
-                    {projectHovered === index && (
-                      <div className="absolute top-[50%] left-[50%] z-[100] transform -translate-x-1/2 -translate-y-1/2 ">
-                        <div className="posting flex flex-col w-full items-center justify-center">
-                          <h1 className="text-4xl font-medium">
-                            {projects.title}
-                          </h1>
-                          <p className="text-3xl pt-3 pb-20">
-                            {projects.stack}
-                          </p>
-                          <button
-                            className="w-[20rem] text-4xl  h-[5rem] border-2  border-[#00abf0] object-cover hover:cursor-pointer hover:bg-[#00abf0] hover:text-[#112e42]"
-                            onClick={() => handlePosts(projects)}
-                          >
-                            LEARN MORE
-                          </button>
-                        </div>
-                      </div>
-                    )}
-                    <Image
-                      src={projects.folder}
-                      alt={projects.name}
-                      width={1920}
-                      height={1080}
-                      className="w-[45rem] h-[25rem]   hover:cursor-pointer border-[0.2px] border-[#00abf0] object-cover hover:opacity-[0.2]"
-                    />
-                  </div>
-                )
-            )}
-          </div>
+          <ProjectsContent
+            post={post}
+            projects={projects}
+            handleMouseLeave={handleMouseLeave}
+            projectHovered={projectHovered}
+            handlePosts={handlePosts}
+            handleHover={handleHover}
+            projectType="frontend"
+            fade={fade}
+          />
         )}
         {fullstack && (
-          <div
-            className={`grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:w-[120rem] mt-24 gap-7 px-10 md:px-0 mb-44  ${
-              fade ? "fade-in" : ""
-            } ${post && "hidden"}`}
-          >
-            {projects.map(
-              (projects, index) =>
-                projects.type === "fullstack" && (
-                  <div
-                    className="imagedet "
-                    onMouseEnter={() => handleHover(index)}
-                    onMouseLeave={handleMouseLeave}
-                    key={projects.name}
-                  >
-                    {/*Project Hovering */}
-                    {projectHovered === index && (
-                      <div className="absolute top-[50%] left-[50%] z-[100] transform -translate-x-1/2 -translate-y-1/2 ">
-                        <div className="posting flex flex-col w-full items-center justify-center">
-                          <h1 className="text-4xl font-medium">
-                            {projects.title}
-                          </h1>
-                          <p className="text-3xl pt-3 pb-20">
-                            {projects.stack}
-                          </p>
-                          <button
-                            className="w-[20rem] text-4xl  h-[5rem] border-2  border-[#00abf0] object-cover hover:cursor-pointer hover:bg-[#00abf0] hover:text-[#112e42]"
-                            onClick={() => handlePosts(projects)}
-                          >
-                            LEARN MORE
-                          </button>
-                        </div>
-                      </div>
-                    )}
-                    <Image
-                      src={projects.folder}
-                      alt={projects.name}
-                      width={1920}
-                      height={1080}
-                      className="w-[45rem] h-[25rem]   border-[0.2px] border-[#00abf0] object-cover   hover:cursor-pointer hover:opacity-[0.2]"
-                    />
-                  </div>
-                )
-            )}
-          </div>
+          <ProjectsContent
+            post={post}
+            projects={projects}
+            handleMouseLeave={handleMouseLeave}
+            projectHovered={projectHovered}
+            handlePosts={handlePosts}
+            handleHover={handleHover}
+            projectType="fullstack"
+            fade={fade}
+          />
         )}
       </div>
     </section>
