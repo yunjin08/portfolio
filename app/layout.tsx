6b2429fab60c3e002a7276d40b1710f28063cf5d
Yunjin <jed.donaire08@gmail.com>
2024-07-07 20:29:59 +0800

    [Feature][Jed] - Added Meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,16 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="icon" href="/jedicon.png" sizes="any" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#112e42" />
+        <meta name="theme-color" content="#112e42"></meta>
+
+        <meta property="og:title" content="Jed Edison Portfolio"/>
+        <meta property="og:description" content="Philippine Based Experienced Programmer"/>
+        <meta property="og:image" content="https://imgur.com/a/YWoM7E6"/>
+        <meta property="og:url" content="https://jed-edison.com/"/>
+        <meta property="og:type" content="website"/>
       </head>
       <body className={inter.className}>
         <ThemeProvider
