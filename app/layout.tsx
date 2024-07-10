1cc99ddf048df0ed94a8029c598a84911e3b7fca
Yunjin <jed.donaire08@gmail.com>
2024-07-10 10:20:48 +0800

    [Build] - added analytics from vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
 
 import "./globals.css";
 import { ThemeProvider } from "./provider";
@@ -39,6 +40,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
