851332fdb0a22cd832f4d3c2e46e9207985c6aeb
Yunjin <jed.donaire08@gmail.com>
2024-10-03 00:03:11 +0800

    [Bugfix] - fixed the bug where clicking navbar makes the page oversized

--- app/page.tsx
+++ app/page.tsx
@@ -13,8 +13,8 @@ import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
 const Home = () => {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
+    <main className=" bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full overflow-hidden">
         <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
