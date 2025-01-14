8c6c852bfe8731432d1439d515d59694763c0c7a
yunjin08 <jed.donaire08@gmail.com>
2025-01-15 02:28:50 +0800

    change text styling

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -64,12 +64,12 @@ const RecentProjects = () => {
                   />
                 </div>
 
-                <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+                <h1 className="font-bold lg:text-lg md:text-md text-base line-clamp-1">
                   {item.title}
                 </h1>
 
                 <p
-                  className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
+                  className="lg:text-sm lg:font-normal font-light text-sm line-clamp-2"
                   style={{
                     color: "#BEC1DD",
                     margin: "1vh 0",
@@ -94,7 +94,7 @@ const RecentProjects = () => {
                   </div>
 
                   <div className="flex justify-center items-center">
-                    <p className="flex lg:text-xl md:text-xs text-sm text-purple">
+                    <p className="flex lg:text-md text-sm text-purple">
                       Check Live Site
                     </p>
                     <FaLocationArrow className="ms-3" color="#CBACF9" />
