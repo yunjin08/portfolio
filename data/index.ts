601e26fc0c8b7a78f62964cea049ea08cf1e09c8
yunjin08 <jed.donaire08@gmail.com>
2025-01-15 01:02:34 +0800

    change current settings

--- data/index.ts
+++ data/index.ts
@@ -54,8 +54,8 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building a Start Up project",
-    description: "The Barber Proj",
+    title: "Currently learning Cloud Architecture",
+    description: "AWS Certification",
     className: "md:col-span-3 md:row-span-2 border-2 border-[#00abf0]",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
