2adf04e9d7a8eeb99f9ee122a5f2e899b3cbcc48
yunjin08 <jed.donaire08@gmail.com>
2025-04-03 16:30:20 +0800

    test lcp

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -16,7 +16,34 @@ const Hero = () => {
 
   return (
     <section id="hero" className="pb-20 overflow-hidden h-[100svh] md:h-auto">
- hell
+      {/* background grid */}
+      <WebGrid className="top-0 md:-bottom-32" />
+      <div
+        className="w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
+       absolute top-0 left-0 flex-center"
+      >
+        {/* Radial gradient for the container to give a faded look */}
+        <div
+          className="absolute pointer-events-none inset-0 flex-center dark:bg-black-100
+         bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+        />
+      </div>
+
+      <div className="flex-center flex-1 lg:pt-0 gap-12 md:gap-5 lg:flex-row flex-col-reverse relative h-full md:h-[100svh] z-10">
+        <div className="w-1/2 flex-center">
+          <div className="relative mt-36 sm:mt-20 xl:mt-32 lg:mt-0 w-[50vw] lg:w-[35vw] duration-700 hover:scale-[1.02]">
+            <div className="absolute borderImg opacity-0 inset-0 rounded-full border-x-4 border-secondary rotate-infinite"></div>
+            <div className="relative w-full h-full flex-center">
+              <PixelatedImage
+                src="/jed2.webp"
+                width={1280}
+                height={1280}
+                className="rounded-full"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
